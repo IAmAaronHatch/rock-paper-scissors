8a26108dbc1d1f7cd00268a4c655ad53a1a2cc6c
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-04-24 21:14:38 -0700

    Got the mechanics to work, not pretty code, no design yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import RPS from './components/RPS'
 
-function App() {
-  return (
-    <div className="App">
-      Welcome
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <RPS />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/components/RPS.js
+++ src/components/RPS.js
@@ -1,24 +1,98 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
 
 class RPS extends Component {
-    constructor () {
-        super ();
+    constructor (props) {
+        super (props);
 
         this.state = {
             playerScore: 0,
-            compScore: 0,
+            compScore: 0
             
         }
     }
 
-    game = () => {
+    game = (val) => {
+        let cchoice = this.getCompChoice();
+        const playerChoice = val;
+
+        if (playerChoice === 'r' && cchoice === 's') {
+            this.setState((preState) => {
+                return {
+                    playerScore : preState.playerScore + 1
+                }
+            })
+            alert('P Wins')
+        }
+        if (playerChoice === 'r' && cchoice === 'p') {
+            this.setState((preState) => {
+                return {
+                    compScore: preState.compScore + 1
+                }
+            })
+            alert('C Wins')
+        }
+        if (playerChoice === 'r' && cchoice === 'r') {
+            alert('Tie')
+        }
+        if (playerChoice === 's' && cchoice === 's') {
+            alert('Tie')
+        }
+        if (playerChoice === 's' && cchoice === 'p') {
+            this.setState((preState) => {
+                return {
+                    playerScore: preState.playerScore + 1
+                }
+            })
+            alert('P Wins')
+        }
+        if (playerChoice === 's' && cchoice === 'r') {
+            this.setState((preState) => {
+                return {
+                    compScore: preState.compScore + 1
+                }
+            })
+            alert('C Wins')
+        }
+        if (playerChoice === 'p' && cchoice === 's') {
+            this.setState((preState) => {
+                return {
+                    compScore: preState.compScore + 1
+                }
+            })
+            alert('C Wins')
+        }
+        if (playerChoice === 'p' && cchoice === 'p') {
+            alert('Tie')
+        }
+        if (playerChoice === 'p' && cchoice === 'r') {
+            this.setState((preState) => {
+                return {
+                    playerScore: preState.playerScore + 1
+                }
+            })
+            alert('P Wins')
+        }
         
     }
 
+    getCompChoice = () => {
+        const choices = ['r', 'p', 's'];
+        const randomNumber = Math.floor(Math.random() * 3);
+
+        return choices[randomNumber];
+    }
+    
+
     render () {
+        let { playerScore, compScore } = this.state
         return (
             <div>
-                
+                <span> {playerScore} </span>
+                <span> {compScore} </span>
+
+                <button onClick={() => this.game('r')} > Rock </button>
+                <button onClick={() => this.game('p')}> Paper </button>
+                <button onClick={() => this.game('s')} > Scissor </button>
             </div>
         )
     }

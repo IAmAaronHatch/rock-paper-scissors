9af898c9ddb878ebd8fe23451e67eb8f3f84afad
Aaron Hatch <aaron@aarons-mbp.attlocal.net>
2019-04-24 17:31:25 -0700

    added components and basic structure/state to rps comp

---
+++ src/components/RPS.js
@@ -0,0 +1,25 @@
+import react, { Component } from 'react';
+
+class RPS extends Component {
+    constructor () {
+        super ();
+
+        this.state = {
+            playerScore: 0,
+            compScore: 0,
+            
+        }
+    }
+
+
+
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default RPS;
